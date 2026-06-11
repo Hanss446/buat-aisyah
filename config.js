4cd630c45cd3938f2b179f90f53e3e9e3d0dd9fc
HanHan <hanifsannn@gmail.com>
2026-06-11 21:19:47 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    titleWeb: "Teruntuk Aisyah 💌",
+    introTitle: 'Aisyah',
+    introDesc: `Halooo Halooo, dari suami kamu...
+    Yang baru bisa bilang makasih
+    Setelah baca semuanya berulang ii 🥺`,
+    btnIntro: 'Ih apaan sih 🙈',
+    title: 'Kamu tau gak sih, aku baca itu sampe berulang ii sayangg hahahaha 🥺',
+    desc: 'Dari halaman pertama sampe terakhir. Makasih udah nulis sepanjang itu buat aku, yang kadang ga selalu bisa ngungkapin balik sebanyak itu.',
+    btnYes: 'Lanjut deh 🩷',
+    btnNo: 'Gausah ah :(',
+    question: 'Kamu bilang aku "pelangi selepas hujan" kamu. Tapi jujur, kamu yang lebih sering bikin hari aku cerah duluan 🌸',
+    btnReply: 'Kirim 💌',
+    reply: 'Makasih udah sayang aku sebanyak itu ya, sampai dibuatin 8 halaman segala yang mana kamu gasuka buat ppt kaya gituu 🩷',
+    mess: 'I love you too Aisyah. More than I usually say out loud. 🥺',
+    messDesc: 'Yuk ngobrol sama suami kamu~ 💬',
+    btnAccept: 'Hiii iyaaa 🌸',
+    messLink: 'https://wa.me/6281225975778'
+}
